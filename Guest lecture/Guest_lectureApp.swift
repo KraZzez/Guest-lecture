87a4da6c92546f5aebcdee666415757f771610cc
KraZzez <ludvig.ludde23@gmail.com>
2022-11-07 10:00:05 +0100

    Initial Commit

---
+++ Guest lecture/Guest_lectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guest_lectureApp.swift
+//  Guest lecture
+//
+//  Created by Ludvig Krantzén on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct Guest_lectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
